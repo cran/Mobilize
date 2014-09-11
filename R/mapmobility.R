57f4ad112a0af2ce89e88e06b3c9abb7e40185eb
Jeroen Ooms <jeroen.ooms@stat.ucla.edu>
2014-09-11 00:00:00 +0000

    version 2.16-4

--- R/mapmobility.R
+++ R/mapmobility.R
@@ -1,6 +1,5 @@
 mapmobility <- function(date, username,...){
-	library(maps);
 	mobility <- oh.mobility.read(date=date, username=username,...);
-	map("county", "california");
+	maps::map("county", "california");
 	lines(mobility$lo, mobility$la, type="l", col="red", lwd=10);
 }
